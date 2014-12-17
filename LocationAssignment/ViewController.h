7a3c9f97d11b8837f326b4e2089744bcbcf1a626
admin <admin@UPT-1.local>
2014-12-17 10:00:53 +0530

    Initial Commit

---
+++ LocationAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocationAssignment
+//
+//  Created by admin on 17/12/14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
